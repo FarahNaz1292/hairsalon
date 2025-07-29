3154bbd730bd0bbe848c95a1c1247c3052aa49bf
Farah naz <farahnaz@Mac.lan>
2025-07-29 13:43:50 -0400

    added responsiveness

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { MenuIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -18,35 +20,70 @@ const Navbar = () => {
                         height={10}
                     />
                 </div>
-
-                {/* Navigation Links */}
-                <div>
-                    <Button asChild variant="ghost" className='font-extrabold'>
-                        <Link href="/services">SERVICES</Link>
-                    </Button>
-                    <Button asChild variant="ghost" className='font-extrabold'>
-                        <Link href="/about">ABOUT</Link>
-                    </Button>
-                    <Button asChild variant="ghost" className='font-extrabold'>
-                        <Link href="/specials">SPECIALS</Link>
-                    </Button>
-                    <Button asChild variant="ghost" className='font-extrabold'>
-                        <Link href="/shop">SHOP</Link>
-                    </Button>
-                    <Button asChild variant="ghost" className='font-extrabold'>
-                        <Link href="/locations">LOCATIONS</Link>
-                    </Button>
+                {/* Desktop Nav */}
+                <div className='hidden md:flex flex-col items-center justify-center h-full'>
+                    <div>
+                        <Button asChild variant="ghost" className='font-extrabold text-lg'>
+                            <Link href="/services">SERVICES</Link>
+                        </Button>
+                        <Button asChild variant="ghost" className='font-extrabold text-lg'>
+                            <Link href="/about">ABOUT</Link>
+                        </Button>
+                        <Button asChild variant="ghost" className='font-extrabold text-lg'>
+                            <Link href="/specials">SPECIALS</Link>
+                        </Button>
+                        <Button asChild variant="ghost" className='font-extrabold text-lg'>
+                            <Link href="/shop">SHOP</Link>
+                        </Button>
+                        <Button asChild variant="ghost" className='font-extrabold text-lg'>
+                            <Link href="/locations">LOCATIONS</Link>
+                        </Button>
+                    </div>
+                    <div className='absolute bottom-4 right-1/9 flex gap-4'>
+                        <Button asChild variant="ghost" className='font-extrabold text-xl'>
+                            <Link href="/booking">BOOK APPOINTMENT</Link>
+                        </Button>
+                        <Button asChild variant="ghost" className='font-extrabold text-xl'>
+                            <Link href="/giftcard">GIFT CARD</Link>
+                        </Button>
+                    </div>
                 </div>
-            </div>
+                {/* Mobile Nav */}
+                <div className='md:hidden'>
+                    <Sheet >
+                        <SheetTrigger asChild>
+                            <Button variant="ghost" size="icon">
+                                <MenuIcon className='w-6 h-6' />
+                            </Button>
 
-            {/* Bottom-Right Button Group */}
-            <div className='absolute bottom-4 right-1/9 flex gap-4'>
-                <Button asChild variant="ghost" className='font-extrabold'>
-                    <Link href="/booking">BOOK APPOINTMENT</Link>
-                </Button>
-                <Button asChild variant="ghost" className='font-extrabold'>
-                    <Link href="/giftcard">GIFT CARD</Link>
-                </Button>
+                        </SheetTrigger>
+                        <SheetContent>
+                            <div className='flex flex-col gap-4 p-4 bg-gray-600 h-full text-white'>
+                                <Button asChild variant="ghost" className='font-extrabold text-xl'>
+                                    <Link href="/services">SERVICES</Link>
+                                </Button>
+                                <Button asChild variant="ghost" className='font-extrabold text-xl'>
+                                    <Link href="/about">ABOUT</Link>
+                                </Button>
+                                <Button asChild variant="ghost" className='font-extrabold text-xl'>
+                                    <Link href="/specials">SPECIALS</Link>
+                                </Button>
+                                <Button asChild variant="ghost" className='font-extrabold text-xl'>
+                                    <Link href="/shop">SHOP</Link>
+                                </Button>
+                                <Button asChild variant="ghost" className='font-extrabold text-xl'>
+                                    <Link href="/locations">LOCATIONS</Link>
+                                </Button>
+                                <Button asChild variant="ghost" className='font-extrabold text-xl'>
+                                    <Link href="/booking">BOOK APPOINTMENT</Link>
+                                </Button>
+                                <Button asChild variant="ghost" className='font-extrabold text-xl'>
+                                    <Link href="/giftcard">GIFT CARD</Link>
+                                </Button>
+                            </div>
+                        </SheetContent>
+                    </Sheet>
+                </div>
             </div>
         </div>
     )
